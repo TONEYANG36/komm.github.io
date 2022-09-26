56f132a03120e0baa104354bde39fadf8e1b0558
kommst <1826335281@qq.com>
2022-09-26 21:04:34 +0800

    Site updated: 2022-09-26 21:04:23

---
+++ dist/music.js
@@ -0,0 +1,69 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    order: 'random',
+    audio: [
+      {
+        name: "Komm",
+        artist: 'Loop_music',
+        url: '/medias/featureimages/1.mp3',
+        cover: 'https://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+      },
+      {
+        name: '柏大輔 - A Rainy Morning ~Main Title~',
+        artist: 'Loop_music',
+        url: '/medias/featureimages/柏大輔 - A Rainy Morning ~Main Title~.mp3',
+        cover: 'https://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+      {
+        name: '柏大輔 - The Afternoon Of Rainy day',
+        artist: 'Loop_music',
+        url: '/medias/featureimages/柏大輔 - The Afternoon Of Rainy day.mp3',
+        cover: 'https://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+      {
+        name: '岡部啓一 - 穏ヤカナ眠リ',
+        artist: 'Loop_music',
+        url: '/medias/featureimages/岡部啓一 - 穏ヤカナ眠リ.mp3',
+        cover: 'https://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+      {
+        name: '岡部啓一 - 遺サレタ場所／斜光',
+        artist: 'Loop_music',
+        url: '/medias/featureimages/岡部啓一 - 遺サレタ場所／斜光.mp3',
+        cover: 'https://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+      {
+        name: '网易云音乐校园,CMJ - 银河赴约',
+        artist: 'Loop_music',
+        url: '/medias/featureimages/网易云音乐校园,CMJ - 银河赴约.mp3',
+        cover: 'https://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+      {
+        name: '祖堅正慶 - スカイロード 〜ドラヴァニア雲海：夜〜',
+        artist: 'Loop_music',
+        url: '/medias/featureimages/祖堅正慶 - スカイロード 〜ドラヴァニア雲海：夜〜.mp3',
+        cover: 'https://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+            {
+        name: '祖堅正慶 - 黒闇と白雪 〜クルザス西部高地：夜〜',
+        artist: 'Loop_music',
+        url: '/medias/featureimages/祖堅正慶 - 黒闇と白雪 〜クルザス西部高地：夜〜.mp3',
+        cover: 'https://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+            {
+        name: '祖堅正慶 - 静穏の森 ~黒衣森フィールド~',
+        artist: 'Loop_music',
+        url: '/medias/featureimages/祖堅正慶 - 静穏の森 ~黒衣森フィールド~.mp3',
+        cover: 'https://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+            {
+        name: '祖堅正慶 - 天上の世界 〜アバラシア雲海：夜〜',
+        artist: 'Loop_music',
+        url: '/medias/featureimages/祖堅正慶 - 天上の世界 〜アバラシア雲海：夜〜.mp3',
+        cover: 'https://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+
+    ]
+});
